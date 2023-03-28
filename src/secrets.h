e9c742f3198b7b0fa83e2cf7d5cfac6fb4ebd85e
AdrianVispalia <adrianvispaliamoreno@gmail.com>
2023-03-28 02:56:51 +0100

    deleted secrets.h

--- src/secrets.h
+++
@@ -1,9 +0,0 @@
-// Needed env variables:
-// #define AP_SSID
-// #define AP_PASSWORD
-
-// Optional env variables (for Telegram bot)
-// #define WIFI_SSID
-// #define WIFI_PASSWORD
-// #define BOTtoken
-// #define CHAT_ID
