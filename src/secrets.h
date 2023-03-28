38a1b0657eaaad15c66c971b52728c13396ee4d4
AdrianVispalia <125102300+AdrianVispalia@users.noreply.github.com>
2023-03-28 02:40:30 +0100

    Create secrets.h

---
+++ src/secrets.h
@@ -0,0 +1,9 @@
+// Needed env variables:
+// #define AP_SSID
+// #define AP_PASSWORD
+
+// Optional env variables (for Telegram bot)
+// #define WIFI_SSID
+// #define WIFI_PASSWORD
+// #define BOTtoken
+// #define CHAT_ID
